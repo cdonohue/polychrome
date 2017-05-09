a852d598795104d239a3b917b866123e79033ebb
Chad Donohue <chad.donohue@gmail.com>
2017-05-08 21:52:42 -0500

    Add relative luminance

---
+++ src/relativeLuminance.js
@@ -0,0 +1,10 @@
+export default color => {
+  const [R, G, B] = [color.r, color.g, color.b].map(channel => {
+    const sRGB = channel / 255;
+    return sRGB <= 0.03928
+      ? sRGB / 12.92
+      : Math.pow((sRGB + 0.055) / 1.055, 2.4);
+  });
+
+  return 0.2126 * R + 0.7152 * G + 0.0722 * B;
+};
